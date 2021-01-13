c586a7f50407eae81bebffd30de0e3f1929b4fc7
Andrea Esposito <ryuesposito@gmail.com>
2021-01-13 13:47:40 +0100

    It now reads the user input instead of args.

--- echoServerRust/src/client.rs
+++ echoServerRust/src/client.rs
@@ -1,6 +1,8 @@
+// cargo run --bin echo-client
 use proto::echo_client::EchoClient;
 use proto::Message;
-use std::env;
+//use std::env; //used to get the env variables
+use std::io::{self, Write};
 
 pub mod proto {
     tonic::include_proto!("proto"); // Echo.proto package name = proto
@@ -10,10 +12,18 @@ pub mod proto {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = EchoClient::connect("http://[::1]:50051").await?;
 
-    let args: Vec<String> = env::args().collect();
+    //let args: Vec<String> = env::args().collect();
+
+    print!("What are you thinking? ");
+    io::stdout().flush().unwrap();
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    // read_line leaves a trailing newline, which trim will remove
+    let command = input.trim();
 
     let request = tonic::Request::new(Message {
-        content: args[1].as_bytes().into(),
+        content: command.as_bytes().into(),
     });
 
     let response = client.send(request).await?;
