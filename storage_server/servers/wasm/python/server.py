9c14e1856868e7a1cfd5738cbdd397f9f8574fe6
jmcad <jmcadinez@gmail.com>
2021-02-12 16:55:09 +0100

    started working on python storage server, moved pb generated files

---
+++ storage_server/servers/wasm/python/server.py
@@ -0,0 +1,68 @@
+from wasmtime import *
+from concurrent import futures
+import grpc
+import numpy as np 
+
+# Import the generated classes
+import storage_pb2_grpc, storage_pb2
+
+store = Store()
+
+linker = Linker(store)
+wasi_config = WasiConfig()
+wasi_config.preopen_dir("./data", ".")
+wasi = WasiInstance(store, "wasi_snapshot_preview1", wasi_config)
+linker.define_wasi(wasi)
+
+# Load and compile the WebAssembly-module
+module_linking = Module.from_file(store.engine, "storage_server/servers/wasm/wasm_module/storage_application.wasm")
+
+# Instantiate the module which only uses WASI
+instance_linking = linker.instantiate(module_linking)
+
+# Execute the _initialize function to give WASM access
+# to the data folder
+init = instance_linking.exports["_initialize"]
+
+# Export functions and memory from the WebAssembly module
+alloc = instance_linking.exports["new_alloc"]
+dealloc = instance_linking.exports["new_dealloc"]
+get_len = instance_linking.exports["get_response_len"]
+write = instance_linking.exports["store_data"]
+read = instance_linking.exports["read_data"]
+mem = instance_linking.exports["memory"]
+
+# gRPC related variables
+grpc_host = u'[::]'
+grpc_port = u'50051'
+grpc_address = u'{host}:{port}'.format(host=grpc_host, port=grpc_port)
+
+# copy_mem handles the copy of serialized data to the
+# Wasm's memory
+def copy_mem(sdata):
+    return
+    
+
+
+# create a class to define the server functions, derived
+# from storage_pb2_grpc.StorageServicer
+class StorageServicer(storage_pb2_grpc.StorageServicer):
+
+    def Read(self, request, context):
+        return
+
+    def Write(self, request, context):
+        return
+
+
+# Initialize gRPC server
+def run():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    storage_pb2_grpc.add_StorageServicer_to_server(StorageServicer(), server)
+    server.add_insecure_port(grpc_address)
+    server.start()
+
+if __name__ == "__main__":
+    run()
+
+
