022f84deb5e7ae81d4d4ec9246dae9e5108e05bd
Andrea Esposito <ryuesposito@gmail.com>
2021-02-19 10:55:11 +0100

    Done with the Dotnet implementation

--- storage_server/servers/wasm/DotNetServer/Services/StorageService.cs
+++ storage_server/servers/wasm/DotNetServer/Services/StorageService.cs
@@ -33,17 +33,55 @@ namespace DotNetServer.Services
             }
         }
 
-        // this keeps truck of
+        // this keeps truck of the state of the instace
         public bool instanceReady = false;
 
         // our wasm instance that gets made with lazy initialization 
         public Instance wasm;
         public Wasmtime.Externs.ExternMemory memory;
+
+        // keeps track of the functions used by the applicaion (no alloc, dealloc etc..)
+        public Dictionary<String, Wasmtime.Externs.ExternFunction> funcs;
+
+        public byte[] callFunction(String fn, IMessage message)
+        {
+            // --- Copy the buffer to the module's memory 
+            var bytes = message.ToByteArray();
+
+            var ptr = ((dynamic)wasm).new_alloc(bytes.Length);
+            var len = bytes.Length;
+
+            Console.WriteLine($"I got here and this is the ptr: {ptr}");
+
+            bytes.CopyTo(memory.Span[ptr..]);
+
+
+            // you can technically define the function each and every time
+            //var func = wasm.Functions.Where(f => f.Name == fn).First();
+
+            // call the function you are intrested in
+            var resPtr = funcs[fn].Invoke(ptr, len);
+
+            // deallocate request protobuf message
+            ((dynamic)wasm).new_dealloc(ptr, len);
+
+            // get Wasm response lenght
+            var resultLen = ((dynamic)wasm).get_response_len();
+
+            // copy byte array fom WebAssembly to a result byte array
+            var resultMemory = memory.Span[resPtr..(resPtr + resultLen)];
+            var result = new byte[resultLen];
+            resultMemory.CopyTo(result);
+
+            // deallocate repsonse protobud message form Wasmtime's memory
+            ((dynamic)wasm).new_dealloc(resPtr, resultLen);
+
+            return result;
+        }
     }
     public class StorageService : Storage.StorageBase
     {
         private readonly ILogger<StorageService> _logger;
-
         private WasmSingleton wasmSingleton = WasmSingleton.Instance;
 
 
@@ -52,25 +90,41 @@ namespace DotNetServer.Services
 
             if (!wasmSingleton.instanceReady)
             {
+                // Set up the logger
                 _logger = logger;
+                // initialize the WebAssembly module
                 using var engine = new Engine();
                 using var store = new Store(engine);
 
 
+                // pass access to data directory to this Wasm module
                 WasiConfiguration wasiConfiguration = new WasiConfiguration();
                 wasiConfiguration.WithPreopenedDirectory("./data", ".");
 
+                // Create the WebAssembly-module
                 using var module = Module.FromFile(engine, "../wasm_module/storage_application.wasm");
 
+                // get instance of the wasm module ans specify the version of wasi
                 using var host = new Host(store);
                 host.DefineWasi("wasi_snapshot_preview1", wasiConfiguration);
                 var instance = host.Instantiate(module);
-                var memory = instance.Memories.Where(m => m.Name == "memory").First();
+
+                // execure the _initialize function to given wasm access to the data folder
                 ((dynamic)instance)._initialize();
 
+                // export the only memory that we are using with this module 
+                var memory = instance.Memories.Where(m => m.Name == "memory").First();
 
                 wasmSingleton.wasm = instance;
                 wasmSingleton.memory = memory;
+
+                // export application functions from the WebAssembly module
+                var funcs = new Dictionary<String, Wasmtime.Externs.ExternFunction>();
+                funcs["write"] = wasmSingleton.wasm.Functions.Where(f => f.Name == "store_data").First();
+                funcs["read"] = wasmSingleton.wasm.Functions.Where(f => f.Name == "read_data").First();
+
+
+                wasmSingleton.funcs = funcs;
                 wasmSingleton.instanceReady = true;
 
                 Console.WriteLine("Instance ready");
@@ -84,43 +138,12 @@ namespace DotNetServer.Services
         public override Task<ReadResponse> Read(ReadRequest request, ServerCallContext context)
         {
 
-            var bytes = request.ToByteArray();
-
-            var ptr = ((dynamic)wasmSingleton.wasm).new_alloc(bytes.Length);
-
-            Console.WriteLine($"I got here and this is the ptr: {ptr}");
-
-            bytes.CopyTo(wasmSingleton.memory.Span[ptr..]);
-
-            var len = bytes.Length;
-
-
-            var write = wasmSingleton.wasm.Functions.Where(f => f.Name == "read_data").First();
-
-            //var resptr = ((dynamic)instance).store_data(ptr, len);
-
-            var resptr = write.Invoke(ptr, len);
-
-            ((dynamic)wasmSingleton.wasm).new_dealloc(ptr, len);
-
-            var resultLen = ((dynamic)wasmSingleton.wasm).get_response_len();
-
-            var result = new byte[resultLen];
-
-            var memResult = wasmSingleton.memory.Span[resptr..(resptr + resultLen)];
-
-            memResult.CopyTo(result);
+            var result = wasmSingleton.callFunction("read", request);
 
             ReadResponse resMessage;
-
             resMessage = ReadResponse.Parser.ParseFrom(result);
-
-            Console.WriteLine($"This is the value of message: {resMessage.Value}");
-            Console.WriteLine($"This is the time of message: {resMessage.Timestamp}");
-
-            ((dynamic)wasmSingleton.wasm).new_dealloc(resptr, resultLen);
-
-
+            // Console.WriteLine($"This is the value of message: {resMessage.Value}");
+            // Console.WriteLine($"This is the time of message: {resMessage.Timestamp}");
             return Task.FromResult(resMessage);
 
         }
@@ -128,42 +151,11 @@ namespace DotNetServer.Services
         public override Task<WriteResponse> Write(WriteRequest request, ServerCallContext context)
         {
 
-            var bytes = request.ToByteArray();
-
-            var ptr = ((dynamic)wasmSingleton.wasm).new_alloc(bytes.Length);
-
-            Console.WriteLine($"I got here and this is the ptr: {ptr}");
-
-            bytes.CopyTo(wasmSingleton.memory.Span[ptr..]);
-
-            var len = bytes.Length;
-
-            // Console.WriteLine("I got here");
-
-            var write = wasmSingleton.wasm.Functions.Where(f => f.Name == "store_data").First();
-
-            //var resptr = ((dynamic)instance).store_data(ptr, len);
-
-            var resptr = write.Invoke(ptr, len);
-
-            ((dynamic)wasmSingleton.wasm).new_dealloc(ptr, len);
-
-            var resultLen = ((dynamic)wasmSingleton.wasm).get_response_len();
-
-            var result = new byte[resultLen];
-
-            var memResult = wasmSingleton.memory.Span[resptr..(resptr + resultLen)];
-
-            memResult.CopyTo(result);
+            var result = wasmSingleton.callFunction("write", request);
 
             WriteResponse resMessage;
-
             resMessage = WriteResponse.Parser.ParseFrom(result);
-
-            Console.WriteLine($"This is the status of message: {resMessage.Ok}");
-
-            ((dynamic)wasmSingleton.wasm).new_dealloc(resptr, resultLen);
-
+            //Console.WriteLine($"This is the status of message: {resMessage.Ok}");
             return Task.FromResult(resMessage);
         }
     }
