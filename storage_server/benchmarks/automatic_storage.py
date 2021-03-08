3ed6b4b8e78625357b7b06cced91a040a1c9bdfb
Andrea Esposito <ryuesposito@gmail.com>
2021-03-08 11:44:28 +0100

    Added more sizes of text

--- storage_server/benchmarks/automatic_storage.py
+++ storage_server/benchmarks/automatic_storage.py
@@ -20,11 +20,17 @@ R_PROTO = "../../proto/storage.proto"
 GHZ = "../../../../ghz/cmd/ghz/ghz"
 
 # Write Benchmark message:
-read_file_name = "This is a test"
+read_file_name = "test"
 read = "{\"FileName\":\"" + read_file_name + "\"}"
 
 # Read Benchmark message:
-write_conetent = "This is a test"
+# 10 bytes
+write_conetent = "Testing!!!"
+
+# 1Kb
+_1kb = "Bruce Wayne was born to wealthy physician Dr. Thomas Wayne and his wife Martha, who were themselves members of the prestigious Wayne and Kane families of Gotham City, respectively. When he was three, Bruce's mother Martha was expecting a second child to be named Thomas Wayne, Jr. However, because of her intent to found a school for the underprivileged in Gotham, she was targeted by the manipulative Court of Owls, who arranged for her to have a car accident. She and Bruce survived, but the accident forced Martha into premature labor, and the baby was lost. While on vacation to forget about these events, the Wayne Family butler, Jarvis Pennyworth was killed by one of the Court of Owls' Talons. A letter he'd written to his son Alfred, warning him away from the beleaguered Wayne family, was never delivered. As such, Alfred - who had been an actor at the Globe Theatre at the time and a military medic before that, traveled to Gotham City to take up his father's place, serving the Waynes."
+_10kb = _1kb*10
+
 write = "{\"FileName\":\"test\", \"Value\":\"" + write_conetent + \
     "\", \"Timestamp\":\"" + "2006-01-02T15:04:05.999999999Z" + "\"}"
 
