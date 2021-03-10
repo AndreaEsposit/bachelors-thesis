6c57d191a998f145a569e64eb737edfd4dde620a
Andrea Esposito <ryuesposito@gmail.com>
2021-03-10 21:05:21 +0100

    change

--- storage_server/benchmarks/multi/benchmark.sh
+++ storage_server/benchmarks/multi/benchmark.sh
@@ -1,15 +1,30 @@
-#!/usr/bin/bash
+#!/usr/local/bin/bash
 
-# Run go custom benchmark program 
-read -p "Number of clients:" CLIENTS
-read -p "Number of requests:" REQUESTS
-read -p "Mode:" MODE
+# if it doesn't work: chmod +x benchmark.sh
 
-for i in $(seq 1 $CLIENTS)
-    do
-        nohup go run main.go $REQUESTS $MODE $i >/dev/null 2>&1 &
-done
+# update build on every bbchain (git pull)
+firstBbchain = 20
+lastBbchain = 30
 
-# Acquire data
-# sleep 1m
-# python3 analyse.py
+
+
+for i in $(seq $firstBbchain $lastBbchain);do 
+    ssh jmcad@bbchain{$i}.ux.uis.no
+    cd Practice/
+    git reset --hard HEAD
+    git pull
+
+
+# # Run go custom benchmark program 
+# read -p "Number of clients:" CLIENTS
+# read -p "Number of requests:" REQUESTS
+# read -p "Mode:" MODE
+
+# for i in $(seq 1 $CLIENTS)
+#     do
+#         nohup go run main.go $REQUESTS $MODE $i >/dev/null 2>&1 &
+# done
+
+# # Acquire data
+# # sleep 1m
+# # python3 analyse.py
