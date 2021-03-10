bcae960016544746bfe0b2e9ce2784687d8a6fe4
Andrea Esposito <ryuesposito@gmail.com>
2021-03-10 21:19:21 +0100

    change

--- storage_server/benchmarks/multi/copyKey.sh
+++ storage_server/benchmarks/multi/copyKey.sh
@@ -7,6 +7,6 @@ LB=30
 
 
 for i in $(seq $FB $LB);do 
-    scp ~/.ssh/id_ed25519.pub andreaes@bbchain{$i}.ux.uis.no:~/.ssh
-    scp ~/.ssh/id_ed25519 andreaes@bbchain{$i}.ux.uis.no:~/.ssh
+    scp ~/.ssh/id_ed25519.pub andreaes@bbchain$i.ux.uis.no:~/.ssh
+    scp ~/.ssh/id_ed25519 andreaes@bbchain$i.ux.uis.no:~/.ssh
 done
