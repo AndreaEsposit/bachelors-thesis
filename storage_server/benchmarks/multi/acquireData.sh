33397c1b6b5979b62c332d01493eda898a4c4705
Andrea Esposito <ryuesposito@gmail.com>
2021-03-11 11:24:10 +0100

    change2

--- storage_server/benchmarks/multi/acquireData.sh
+++ storage_server/benchmarks/multi/acquireData.sh
@@ -6,5 +6,5 @@ read -p "Number of first bbchain machine: " FB
 read -p "Number of last bbchain machine: " LB
 
 # scp data to bbchain1
-pdsh -w andreaes@bbchain[$FB-$LB] "cd Practice/storage_server/benchmarks/multi/&&for f in *.csv; do mv '$f' '${f%.txt}_$HOSTNAME.csv'; done&&scp ./*.csv andreaes@bbchain1.ux.uis.no:Practice/storage_server/benchmarks/multi&&rm *.csv"
+pdsh -w andreaes@bbchain[$FB-$LB] "cd Practice/storage_server/benchmarks/multi/&&for f in *.csv; do mv ""$f"" ""${f%.txt}-$HOSTNAME.csv""; done&&scp ./*.csv andreaes@bbchain1.ux.uis.no:Practice/storage_server/benchmarks/multi&&rm *.csv"
 
