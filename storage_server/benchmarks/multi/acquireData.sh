9e1c6c488acf94fe58b6e04db93f9dc19b4ad09d
Andrea Esposito <ryuesposito@gmail.com>
2021-03-16 00:52:56 +0100

    Run python in acquireData.sh

--- storage_server/benchmarks/multi/acquireData.sh
+++ storage_server/benchmarks/multi/acquireData.sh
@@ -10,3 +10,6 @@ for i in $(seq $FB $LB)
     do
         ssh andreaes@bbchain$i.ux.uis.no -n 'cd Practice/storage_server/benchmarks/multi/&&for f in *.csv; do mv "$f" "${f%.csv}_$HOSTNAME.csv"; done; scp ./*.csv andreaes@bbchain1.ux.uis.no:Practice/storage_server/benchmarks/multi&&rm *.csv; exit'
 done
+
+# Runs the python code to analyze the data
+python3 analyse.py
