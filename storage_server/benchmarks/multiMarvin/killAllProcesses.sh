28de9446dced2f0ce5199998930c1d6f55884a39
Andrea Esposito <ryuesposito@gmail.com>
2021-03-23 14:00:49 +0100

    fixed little mistake

--- storage_server/benchmarks/multiMarvin/killAllProcesses.sh
+++ storage_server/benchmarks/multiMarvin/killAllProcesses.sh
@@ -1,6 +1,6 @@
 #!/usr/local/bin/bash
 # remember to: chmod +x killAllProcesses.sh
 
-pdsh -w andreaes@bbchain[1-30] "killall -u jmcad"
+pdsh -w jmcad@bbchain[1-30] "killall -u jmcad"
 
-pdsh -w andreaes@bbchain[1-30] "ps -u jmcad"
+pdsh -w jmcad@bbchain[1-30] "ps -u jmcad"
