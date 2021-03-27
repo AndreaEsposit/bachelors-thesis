e20159a3a101d7e5d6a96358f697340f6502acab
Andrea Esposito <ryuesposito@gmail.com>
2021-03-27 01:27:13 +0100

    Added old Dotnet

--- storage_server/servers/wasm/DotNetServer/Program.cs
+++ storage_server/servers/wasm/DotNetServer/Program.cs
@@ -1,5 +1,11 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace DotNetServer
 {
     public class Program
